f9f2991212b856048e68cfe7fbcf92532906e140
Harrison Melton <harrisonmelton@Harrisons-MacBook-Pro.local>
2017-07-25 22:23:51 -0700

    added back fragments

---
+++ app/src/main/java/com/hmmelton/workouttracker/fragments/HistoryFragment.java
@@ -0,0 +1,44 @@
+package com.hmmelton.workouttracker.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.hmmelton.workouttracker.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link HistoryFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class HistoryFragment extends Fragment {
+
+
+    public HistoryFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment.
+     *
+     * @return A new instance of fragment HistoryFragment.
+     */
+    public static HistoryFragment newInstance() {
+        return new HistoryFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_history, container, false);
+    }
+}
