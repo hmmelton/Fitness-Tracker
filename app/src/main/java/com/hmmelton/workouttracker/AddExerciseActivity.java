f69111790bf9f7d11cf1b6ae1078228ec4d92f0a
Harrison Melton <harrisonmelton@Harrisons-MacBook-Pro.local>
2017-07-25 22:24:30 -0700

    stuff with MainActivity, mostly.  Nothing big

--- app/src/main/java/com/hmmelton/workouttracker/AddExerciseActivity.java
+++ app/src/main/java/com/hmmelton/workouttracker/AddExerciseActivity.java
@@ -3,11 +3,15 @@ package com.hmmelton.workouttracker;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import butterknife.ButterKnife;
+
 public class AddExerciseActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_exercise);
+        // Init Butter Knife
+        ButterKnife.bind(this);
     }
 }
--- app/src/main/java/com/hmmelton/workouttracker/MainActivity.java
+++ app/src/main/java/com/hmmelton/workouttracker/MainActivity.java
@@ -1,7 +1,12 @@
 package com.hmmelton.workouttracker;
 
+import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -12,6 +17,12 @@ import android.widget.Toast;
 
 import com.hmmelton.workouttracker.data.DatabaseUtil;
 import com.hmmelton.workouttracker.data.ExerciseDbHelper;
+import com.hmmelton.workouttracker.fragments.ExercisesFragment;
+import com.hmmelton.workouttracker.fragments.HistoryFragment;
+import com.hmmelton.workouttracker.fragments.WorkoutsFragment;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindString;
 import butterknife.BindView;
@@ -22,16 +33,12 @@ public class MainActivity extends AppCompatActivity {
 
     @SuppressWarnings("unused")
     private final String TAG = getClass().getSimpleName();
-    private ExerciseListAdapter mAdapter;
-    private SQLiteDatabase mDatabase;
 
     // Views
     @BindView(R.id.main_tab_layout)
     TabLayout mTabLayout;
-    @BindView(R.id.main_recycler_view)
-    RecyclerView mRecyclerView;
-    @BindView(R.id.empty_text_view)
-    TextView mEmptyTextView;
+    @BindView(R.id.main_view_pager)
+    ViewPager mViewPager;
 
     // Strings
     @BindString(R.string.history)
@@ -43,26 +50,6 @@ public class MainActivity extends AppCompatActivity {
     @BindString(R.string.unexpected_error)
     String mUnexpectedError;
 
-    // OnClick listeners
-    @OnClick(R.id.main_fab)
-    void onFabClick() {
-        int currentTab = mTabLayout.getSelectedTabPosition();
-        // Action depends on tab position, so check what it is
-        // TODO: fill this in
-        switch (currentTab) {
-            case 0:
-                break;
-            case 1:
-                break;
-            case 2:
-                break;
-            default:
-                // Let user know there has been an unexpected error
-                Toast.makeText(MainActivity.this, mUnexpectedError, Toast.LENGTH_LONG).show();
-                break;
-        }
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -71,66 +58,47 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         // Create and display tabs
         setUpTabs();
-        // Get database
-        getDatabase();
-        // Set up RecyclerView
-        setUpRecyclerView();
+    }
+
+    /**
+     * This method adds tabs to the passed {@link #mViewPager}
+     */
+    private void setUpViewPager() {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        // Add fragments to the adapter
+        adapter.addFragment(HistoryFragment.newInstance());
+        adapter.addFragment(WorkoutsFragment.newInstance());
+        adapter.addFragment(ExercisesFragment.newInstance());
     }
 
     /**
      * This method creates tabs and adds them to the layout's TabLayout.
      */
     private void setUpTabs() {
-        // Add tabs to tab layout
-        mTabLayout.addTab(mTabLayout.newTab().setText(mHistory));
-        mTabLayout.addTab(mTabLayout.newTab().setText(mWorkouts));
-        mTabLayout.addTab(mTabLayout.newTab().setText(mExercises));
-        // Add listener for tab selection
-        mTabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                // Update the adapter's data
-                mAdapter.setDataType(mTabLayout.getSelectedTabPosition());
-                checkAdapterCount();
-            }
+        mTabLayout.setupWithViewPager(mViewPager);
 
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {}
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {}
-        });
     }
 
-    /**
-     * This method sets up {@link #mRecyclerView}
-     */
-    private void setUpRecyclerView() {
-        // Set layout manager
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-        // Create and set adapter
-        mAdapter = new ExerciseListAdapter(DatabaseUtil.getAllHistory(mDatabase), this,
-                mTabLayout.getSelectedTabPosition());
-        mRecyclerView.setAdapter(mAdapter);
+    // Adapter class for view pager tabs
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
 
-        checkAdapterCount();
-    }
-
-    /**
-     * This method hides the screen's 'nothing to display' TextView, if the RecyclerView's adapter
-     * contains items.
-     */
-    private void checkAdapterCount() {
-        if (mAdapter.getItemCount() > 0) {
-            mEmptyTextView.setVisibility(View.GONE);
+        ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
         }
-    }
 
-    /**
-     * This method prepares the database for CRUD requests.
-     */
-    private void getDatabase() {
-        ExerciseDbHelper dbHelper = new ExerciseDbHelper(this);
-        mDatabase = dbHelper.getWritableDatabase();
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        void addFragment(Fragment fragment) {
+            mFragmentList.add(fragment);
+        }
     }
 }
