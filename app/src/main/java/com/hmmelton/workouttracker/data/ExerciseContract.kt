67cbcb0081e32451198f0ecb1a984fe0449422b3
Harrison Melton <harrisonmelton@Harrisons-MacBook-Pro.local>
2017-08-11 00:12:17 -0700

    removed unused database tables and code referring to those tables

--- app/src/main/java/com/hmmelton/workouttracker/data/ExerciseContract.kt
+++ app/src/main/java/com/hmmelton/workouttracker/data/ExerciseContract.kt
@@ -20,42 +20,4 @@ object ExerciseContract {
         val COLUMN_REP_INTENSITY = "repIntensity"
         val COLUMN_TIMESTAMP = "timestamp"
     }
-
-    /**
-     * This class represents an entry in the Workouts table.
-     */
-    object WorkoutEntry : BaseColumns {
-        val TABLE_NAME = "workouts"
-        val COLUMN_NAME = "workoutName"
-        val COLUMN_TIMESTAMP = "timestamp"
-    }
-
-    /**
-     * This class represents an entry in the History table.
-     */
-    object HistoryEntry : BaseColumns {
-        val TABLE_NAME = "history"
-        val COLUMN_NAME = "date"
-        val COLUMN_TIMESTAMP = "timestamp"
-    }
-
-    /**
-     * This class represents an entry in the Exercise/Workout junction table.
-     */
-    object EWJuncEntry : BaseColumns {
-        val TABLE_NAME = "workoutsExercises"
-        val COLUMN_WORKOUT_ID = "workoutId"
-        val COLUMN_EXERCISE_ID = "exerciseId"
-        val COLUMN_TIMESTAMP = "timestamp"
-    }
-
-    /**
-     * This class represents an entry in the History/Exercise junction table.
-     */
-    object HEJuncEntry : BaseColumns {
-        val TABLE_NAME = "historyExercises"
-        val COLUMN_HISTORY_ID = "historyId"
-        val COLUMN_EXERCISE_ID = "exerciseId"
-        val COLUMN_TIMESTAMP = "timestamp"
-    }
 }
--- app/src/main/java/com/hmmelton/workouttracker/data/ExerciseDbHelper.kt
+++ app/src/main/java/com/hmmelton/workouttracker/data/ExerciseDbHelper.kt
@@ -5,10 +5,6 @@ import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 import android.provider.BaseColumns
 import com.hmmelton.workouttracker.data.ExerciseContract.ExerciseEntry
-import com.hmmelton.workouttracker.data.ExerciseContract.WorkoutEntry
-import com.hmmelton.workouttracker.data.ExerciseContract.HistoryEntry
-import com.hmmelton.workouttracker.data.ExerciseContract.EWJuncEntry
-import com.hmmelton.workouttracker.data.ExerciseContract.HEJuncEntry
 
 /**
  * Created by harrisonmelton on 7/18/17.
@@ -29,61 +25,13 @@ class ExerciseDbHelper// Constructor
                 ExerciseEntry.COLUMN_TIMESTAMP + " TIMESTAMP DEFAULT CURRENT_TIMESTAMP" +
                 "); "
 
-        // String to create Workout table
-        val SQL_CREATE_WORKOUT_TABLE = "CREATE TABLE " + WorkoutEntry.TABLE_NAME + " (" +
-                BaseColumns._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                WorkoutEntry.COLUMN_NAME + " TEXT NOT NULL, " +
-                WorkoutEntry.COLUMN_TIMESTAMP + " TIMESTAMP DEFAULT CURRENT_TIMESTAMP" +
-                "); "
-
-        // String to create History table
-        val SQL_CREATE_HISTORY_TABLE = "CREATE TABLE " + HistoryEntry.TABLE_NAME + " (" +
-                BaseColumns._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                HistoryEntry.COLUMN_NAME + " TEXT NOT NULL, " +
-                HistoryEntry.COLUMN_TIMESTAMP + " TIMESTAMP DEFAULT CURRENT_TIMESTAMP" +
-                "); "
-
-        // String to create Exercise/Workout junction table
-        val SQL_CREATE_EXERCISE_WORKOUT_JUNCTION_TABLE = "CREATE TABLE " +
-                EWJuncEntry.TABLE_NAME + " (" +
-                BaseColumns._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                EWJuncEntry.COLUMN_EXERCISE_ID + " INTEGER NOT NULL, " +
-                EWJuncEntry.COLUMN_WORKOUT_ID + " INTEGER NOT NULL, " +
-                EWJuncEntry.COLUMN_TIMESTAMP +
-                " TIMESTAMP DEFAULT CURRENT_TIMESTAMP, " +
-                "FOREIGN KEY (" + EWJuncEntry.COLUMN_EXERCISE_ID +
-                ") REFERENCES " + ExerciseEntry.TABLE_NAME + "(" + BaseColumns._ID + "), " +
-                "FOREIGN KEY (" + EWJuncEntry.COLUMN_WORKOUT_ID +
-                ") REFERENCES " + WorkoutEntry.TABLE_NAME + "(" + BaseColumns._ID + "));"
-
-        // String to create History/Exercise junction table
-        val SQL_CREATE_HISTORY_EXERCISE_JUNCTION_TABLE = "CREATE TABLE " +
-                HEJuncEntry.TABLE_NAME + " (" +
-                BaseColumns._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                HEJuncEntry.COLUMN_HISTORY_ID + " INTEGER NOT NULL, " +
-                HEJuncEntry.COLUMN_EXERCISE_ID + " INTEGER NOT NULL, " +
-                HEJuncEntry.COLUMN_TIMESTAMP +
-                " TIMESTAMP DEFAULT CURRENT_TIMESTAMP, " +
-                "FOREIGN KEY (" + HEJuncEntry.COLUMN_HISTORY_ID +
-                ") REFERENCES " + HistoryEntry.TABLE_NAME + "(" + BaseColumns._ID + "), " +
-                "FOREIGN KEY (" + HEJuncEntry.COLUMN_EXERCISE_ID +
-                ") REFERENCES " + ExerciseEntry.TABLE_NAME + "(" + BaseColumns._ID + "));"
-
         // Execute SQL commands
         db.execSQL(SQL_CREATE_EXERCISE_TABLE)
-        db.execSQL(SQL_CREATE_WORKOUT_TABLE)
-        db.execSQL(SQL_CREATE_HISTORY_TABLE)
-        db.execSQL(SQL_CREATE_EXERCISE_WORKOUT_JUNCTION_TABLE)
-        db.execSQL(SQL_CREATE_HISTORY_EXERCISE_JUNCTION_TABLE)
     }
 
     override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
         // If DATABASE_VERSION changes, the database tables are updated.
         db.execSQL("ALTER IF TABLE EXISTS " + ExerciseEntry.TABLE_NAME)
-        db.execSQL("ALTER IF TABLE EXISTS " + WorkoutEntry.TABLE_NAME)
-        db.execSQL("ALTER IF TABLE EXISTS " + HistoryEntry.TABLE_NAME)
-        db.execSQL("ALTER IF TABLE EXISTS " + EWJuncEntry.TABLE_NAME)
-        db.execSQL("ALTER IF TABLE EXISTS " + HEJuncEntry.TABLE_NAME)
         onCreate(db)
     }
 
